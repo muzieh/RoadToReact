9f35b38467b95cd63e641e35c68a92b09418aaf0
Muzieh <muzieh@gmail.com>
2020-08-30 07:54:00 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <h1>test</h1>,
+  rootElement
+);
