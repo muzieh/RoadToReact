4c49c649f5e669f14ed34317cfc8ee8a2ba53b7c
Muzieh <muzieh@gmail.com>
2020-09-04 09:02:49 +0200

    useSemiPerstistentState CustomHook

--- src/App.js
+++ src/App.js
@@ -2,49 +2,42 @@ import React from "react";
 import "./styles.css";
 import Search from "./Search";
 
-const useSemitPersistentState = (key, initialValue) => {
+const useSemiPersistentState = (key, initialValue) => {
+  console.log(`seSemiPersistentState key:${key} initialValue:${initialValue}`);
   const [value, setValue] = React.useState(
     localStorage.getItem(key) || initialValue
   );
 
-    React.useEffect(() => {
-      localStorage.setItem(key, value);
-    },[key, value])
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [key, value]);
 
-    return [value, setValue];
+  return [value, setValue];
 };
 
-const Item = ({guid, title, done}) => {
-return (
-  <li>
-    {title}
-    <input type="checkbox" value={done} />
-  </li>  
-);
-}
-const List = ({todos}) => {
+const Item = ({ guid, title, done }) => {
+  return (
+    <li>
+      {title}
+      <input type="checkbox" value={done} />
+    </li>
+  );
+};
+const List = ({ todos }) => {
   return (
     <ul>
-      {todos.map(({guid,...todo}) => (
-          <Item key={guid} {...todo} />
+      {todos.map(({ guid, ...todo }) => (
+        <Item key={guid} {...todo} />
       ))}
     </ul>
   );
 };
 
-
 export default function App(props) {
-  const [searchTerm, setSearchTerm] = useSemitPersistentState('search', 'React');
-  /*
-  const [searchTerm, setSearchTerm] = React.useState(
-    localStorage.getItem('search') || "React");
-  
-  React.useEffect(() => {
-    console.log('useEffect ' +searchTerm);
-    localStorage.setItem('search',searchTerm )
-    
-    }, [searchTerm]);
-*/
+  const [searchTerm, setSearchTerm] = useSemiPersistentState(
+    "search",
+    "Reactor"
+  );
   const handleSearchTerm = (searchTerm) => {
     setSearchTerm(searchTerm);
   };
